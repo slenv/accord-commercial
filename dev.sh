66a7edb8c0a124424b302d3afec64e40dde5ac9e
Lino <kanethplayer@gmail.com>
2025-08-12 12:17:48 -0500

    improves

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar API en una nueva terminal
-gnome-terminal -- bash -c "cd api && php artisan serve; exec bash"
+gnome-terminal -- bash -c "cd api && php artisan serve --host 0.0.0.0; exec bash"
 
 # Iniciar web en otra nueva terminal
 gnome-terminal -- bash -c "cd web && source ~/.nvm/nvm.sh && nvm use 14 && bun start; exec bash"
