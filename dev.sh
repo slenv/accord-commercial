dbcf064d951f61cbc5e100b6958e1e3892962742
Lino <kanethplayer@gmail.com>
2025-12-01 12:26:49 -0500

    chore: update web frontend dev scripts to use pnpm instead of bun

--- dev.sh
+++ dev.sh
@@ -4,4 +4,4 @@
 gnome-terminal -- bash -c "cd api && php artisan serve --host 0.0.0.0; exec bash"
 
 # Iniciar web en otra nueva terminal
-gnome-terminal -- bash -c "cd web && source ~/.nvm/nvm.sh && nvm use 14 && bun start; exec bash"
+gnome-terminal -- bash -c "cd web && source ~/.nvm/nvm.sh && nvm use 14 && pnpm start; exec bash"
